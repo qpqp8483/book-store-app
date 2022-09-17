ee5616627e8f3ee4aeb8bfeba6ce348fd5a5d4e4
chanwoo <wkdcksdn1996@gmail.com>
2022-09-17 15:56:10 +0900

    queryData 설정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,22 @@ import BookItem from "../components/bookItem/BookItem";
 import { bookSearch } from "../KakaoApi";
 import { DiaryDispatchContext } from "../App";
 import "./home.scss";
-
+import { useRoutes, useSearchParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router";
+import QueryString from "qs";
 const Home = () => {
   const { num, setNum } = useContext(DiaryDispatchContext);
   const [books, setBooks] = useState([]); // 책 리스트 state 기본값 설정
   const [listUpdate, setListUpdate] = useState(true); // 책 리스트 업데이트될때 상태변환 설정
   const [queryText, setQueryText] = useState("");
   const [text, setText] = useState("");
-
+  const params = useParams();
+  const location = useLocation();
+  const queryData = QueryString.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  console.log(queryData);
+  console.log(params, location);
   useEffect(() => {
     if (queryText.length > 0) {
       kakaoSearch(queryText, true); // 컴포넌트 마운트 후에, 함수를 호출한다.
@@ -21,7 +29,7 @@ const Home = () => {
     const params = {
       query: query,
       sort: "accuracy", // accuracy | recency 정확도 or 최신
-      page: 1,
+      page: queryData.page,
       size: 10, // 한 페이지에 보여 질 문서의 개수
     };
     const { data } = await bookSearch(params); // api 호출
@@ -64,6 +72,9 @@ const Home = () => {
           ))}
         </div>
       </div>
+      <ul>
+        <li></li>
+      </ul>
     </div>
   );
 };
